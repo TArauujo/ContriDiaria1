d60c5117410032d75cb70f93bd31d200cb214927
thomas <thomascaraujo19@gmail.com>
2024-01-13 01:26:26 -0300

    aprov

--- click/clickp/src/App.js
+++ click/clickp/src/App.js
@@ -1,14 +1,38 @@
 import './App.css';
 import { useState } from 'react';
 
-handleClick = () => {
-  
-}
-
 function App() {
+
+  const [list, setList] = useState([]);
+  const [undid, setUndid] = useState([]);
+
+
+  const handleClick = (event) => {
+
+    const newDot = {
+      clientX: event.clientX,
+      clientY: event.clientY,
+    };
+
+    console.log(newDot);
+    setList((prev) => [...prev, newDot]);
+    setUndid([]);
+  }
+
+ 
+
+
   return (
-    <div id= "page" onClick = {handleClick}>
+    <div id= 'page' onClick = {handleClick}>
       <h1>Ponto onde se clica ai fica uma bolinha onde clicou entendeu?</h1>
+      {JSON.stringify(list)}
+      {list.map((item, index) => (
+        <span 
+          key={index}
+          className='dot'
+          style={{left: item.clientX, top: item.clientY, borderRadius: '50%'}}
+        />
+      ))}
       
     </div>
   );
