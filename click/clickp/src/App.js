6497b8af15ee7ad42f5a18415865ea037e1e390d
thomas <thomascaraujo19@gmail.com>
2024-01-13 00:50:51 -0300

    aprov

---
+++ click/clickp/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { useState } from 'react';
+
+handleClick = () => {
+  
+}
+
+function App() {
+  return (
+    <div id= "page" onClick = {handleClick}>
+      <h1>Ponto onde se clica ai fica uma bolinha onde clicou entendeu?</h1>
+      
+    </div>
+  );
+}
+
+export default App;
