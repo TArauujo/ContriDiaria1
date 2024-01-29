ec512ae0687ac06112126370b33af56eca5b429e
thomas <thomascaraujo19@gmail.com>
2024-01-28 22:44:57 -0300

    criando a pasta com o path

--- Node/express/modules/fs.js
+++ Node/express/modules/fs.js
@@ -1,1 +1,10 @@
 const fs = require('fs');
+const path = require('path');
+
+fs.mkdir(path.join(__dirname, '/test'), (error) => {
+    if(error){
+        return console.log("Erro: ",error)
+    }
+
+    console.log("Pasta criada com sucecsso!!")
+})
