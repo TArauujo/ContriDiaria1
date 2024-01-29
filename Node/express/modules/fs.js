937c846db8dcff2462f07e2eb3a18224f5e970f2
thomas <thomascaraujo19@gmail.com>
2024-01-28 22:48:59 -0300

    criando um arquivo com fs e path

--- Node/express/modules/fs.js
+++ Node/express/modules/fs.js
@@ -1,10 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
+// Criar uma pasta
+
 fs.mkdir(path.join(__dirname, '/test'), (error) => {
     if(error){
         return console.log("Erro: ",error)
     }
 
     console.log("Pasta criada com sucecsso!!")
+})
+
+// Criar um arquivo
+
+fs.writeFile(path.join(__dirname, '/test', 'test.txt'), 'Hello, Node!!', (error) =>{
+    if(error){
+        return console.log("Erro: ",error)
+    }
+    console.log("Arquivo criados com sucesso!!")
 })
