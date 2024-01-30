52f0eeb07553fcb60c3beb1065c86f80bf703079
thomas <thomascaraujo19@gmail.com>
2024-01-30 17:12:17 -0300

    aprov

--- Node/express/modules/http.js
+++ Node/express/modules/http.js
@@ -7,6 +7,22 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { "Content-Type" : "text.html" })
         res.end("<h1>Home Paga</h1>");
     }
+
+    if(req.url === '/users'){
+        const users = [
+            {
+                name: "John Doe",
+                email: "John@doe.com"
+            },
+            {
+                name: "Jane Doe",
+                email: "Jane@doe.com",
+            }
+        ];
+
+        res.writeHead(200, {"Content-Type" : "application/json" });
+        res.end(JSON.stringify(users));
+    }
 });
 
 server.listen(port, () => console.log(`Rodando na porta ${port}!`));
