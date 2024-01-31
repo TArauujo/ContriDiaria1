86355a664f4569b30f6a7a100c973b0661c38a5c
thomas <thomascaraujo19@gmail.com>
2024-01-30 23:47:43 -0300

    aprov

--- Node/express/index.js
+++ Node/express/index.js
@@ -2,5 +2,7 @@ const { Person } = require("./person");
 
 //require('./modules/path');
 //require('./modules/fs');
-require('./modules/http')
+// require('./modules/http')
+
+require('./modules/express.js')
 const person = new Person("Thomas");
