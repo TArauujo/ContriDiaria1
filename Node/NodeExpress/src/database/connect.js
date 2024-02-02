e7f9d6b5307c4e034f154569e1b5cdc99bc9c4f9
Thomas Carvalho <thomascaraujo19@gmail.com>
2024-02-01 23:53:43 -0300

    aprov

--- Node/NodeExpress/src/database/connect.js
+++ Node/NodeExpress/src/database/connect.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 
 const connectTo Database = async () => {
-    await mongoose.connect('mongodb+srv://admin:<password>@cluster0.syfehj8.mongodb.net/?retryWrites=true&w=majority', () => {
-        
+    await mongoose.connect('mongodb+srv://admin:<password>@cluster0.syfehj8.mongodb.net/?retryWrites=true&w=majority', (error) => {
+    (error) => {
+        if(error){
+            return console.log("Ocorreu um erro ao conectar com o banco de dados: ", error)
+        };
+    }
+    return console.log("Conexão ao banco de dados realizada com sucesso!!");
     })
 }
+
+module.exports = connectToDatabase;
