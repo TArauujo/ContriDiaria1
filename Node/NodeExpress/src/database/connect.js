ce7e4037b9e26245b32aa3d5387cf481207d04ec
Thomas Carvalho <thomascaraujo19@gmail.com>
2024-02-01 23:52:03 -0300

    criando a conexão com o db

---
+++ Node/NodeExpress/src/database/connect.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const connectTo Database = async () => {
+    await mongoose.connect('mongodb+srv://admin:<password>@cluster0.syfehj8.mongodb.net/?retryWrites=true&w=majority', () => {
+        
+    })
+}
